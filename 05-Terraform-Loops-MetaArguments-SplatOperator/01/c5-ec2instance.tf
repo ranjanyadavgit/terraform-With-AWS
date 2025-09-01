f1d6a98417a4b00aa951efbadf0789b20f8bb6e9
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-09-01 21:09:02 +0530

    Create c5-ec2instance.tf

---
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/01/c5-ec2instance.tf
@@ -0,0 +1,21 @@
+# EC2 instance
+
+resource "aws_instance" "mysec2vm" {
+    ami = data.aws_ami.amzlinux2.id
+
+    #instance_type=var.instance_type_list[1]
+    instance_type = var.instance_type
+    #instance_type = var.instance_type_map["prod"]
+    user_data = file("${path.module}/app1-install.sh")
+    key_name = var.instance_keypair
+    vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
+    count = 2
+    tags={
+        "Name"="Count-demo-${count.index}"
+    }
+  
+
+}
+
+
+
