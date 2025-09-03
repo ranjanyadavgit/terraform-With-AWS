9d9cd0cbe5cb6e1dc98cee203ac3431e32eeb05b
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-09-03 18:15:03 +0530

    Create c1-version.tf

---
+++ 06/v1-vpc-module/c1-version.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = "~> 1.12.2"
+
+  required_providers {
+     aws = {
+        source = "hashicorp/aws"
+        version = "6.11.0"
+     }
+  }
+}
+
+
+# provider version
+provider "aws" {
+   region = var.aws_region
+}
